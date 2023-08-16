24dc7b5f9f0f120f973c4319e6832c685e3bfc5d
Stephanie Peñaloza Cortés <spenalozacortes@gmail.com>
2023-08-16 13:27:23 -0600

    Add BootRepository

---
+++ java/BootsBoutique/src/main/java/com/codecademy/boots/repositories/BootRepository.java
@@ -0,0 +1,11 @@
+package com.codecademy.boots.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codecademy.boots.entities.Boot;
+import com.codecademy.boots.enums.BootType;
+
+public interface BootRepository extends CrudRepository<Boot, Integer> {
+}
