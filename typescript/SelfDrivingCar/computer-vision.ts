768b0e0d400166c401512f5039f20c36b87a3032
spenalozacortes <spenalozacortes@gmail.com>
2024-05-01 18:17:16 -0600

    Add project

---
+++ typescript/SelfDrivingCar/computer-vision.ts
@@ -0,0 +1,8 @@
+export function getObstacleEvents() {
+    const coinFlip = Boolean(Math.random() > 0.5);
+    return { 
+      'ObstacleLeft': coinFlip, 
+      'ObstacleRight': !coinFlip 
+    };
+  }
+  
