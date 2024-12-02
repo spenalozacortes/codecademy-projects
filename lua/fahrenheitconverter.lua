2f388300d731ac82cd8d0740e8c37adfa8601eb5
Stephanie Peñaloza <spenalozacortes@gmail.com>
2024-12-02 14:31:26 -0600

    add project

---
+++ lua/fahrenheitconverter.lua
@@ -0,0 +1,26 @@
+function fahrenheitToCelsius(degrees)
+  offsetDegrees = degrees - 32
+  conversionFactor = 5 / 9;
+  return offsetDegrees * conversionFactor
+end
+
+function fahrenheitToKelvin(degrees)
+  degreesCelsius = fahrenheitToCelsius(degrees)
+  return degreesCelsius + 273.15
+end
+
+function convertTemperature(targetUnit, degrees)
+  correctedString = string.lower(targetUnit)
+  if correctedString == "kelvin" then
+    return fahrenheitToKelvin(degrees)
+  elseif correctedString == "celsius" then
+    return fahrenheitToCelsius(degrees)
+  else
+    print("Invalid Input")
+    return nil
+  end
+end
+
+inputDegrees = 23
+convertedDegrees = convertTemperature("Kelvin", inputDegrees)
+print(convertedDegrees)
